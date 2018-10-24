96c0e82f4c249656f5f34c7d06775e5e76f5465e
web-dave <dave@webdave.de>
2018-10-24 07:31:02 +0200

    heros_1

--- heros/heros.js
+++ heros/heros.js
@@ -1,1 +1,7 @@
-(function() {})();
+(function() {
+  let xhttp = new XMLHttpRequest();
+  let url =
+    "https://raw.githubusercontent.com/web-dave/JavaScript-Essentials-Training/arrays-and-objects/heros/heros.json";
+  let table = document.querySelector("tbody");
+  let results;
+})();
