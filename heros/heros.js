022c725cc4a20e00767ce5de677e5f9d4c7cf230
web-dave <dave@webdave.de>
2018-10-24 14:27:25 +0200

    final grid

--- heros/heros.js
+++ heros/heros.js
@@ -1,24 +1,22 @@
 (function() {
-  let xhttp = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   let url =
     "https://raw.githubusercontent.com/web-dave/JavaScript-Essentials-Training/arrays-and-objects/heros/heros.json";
-  let table = document.querySelector("tbody");
+  let table = document.querySelector(".heros tbody");
+  let allHeros;
   let results;
 
-  let createRow = data => {
-    return `<tr>
-            <th scope="row">${data.id}</th>
-            <td>${data.name}</td>
-            <td>${data.description}</td>
-            <td>${data.comics.returned}</td>
-        </tr>`;
-  };
-
   let getData = () => {
-    registerSort();
-    xhttp.open("GET", url, true);
-    xhttp.send();
-    xhttp.onreadystatechange = function() {
+    xhr.open("GET", url, true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      // handle Response
+      // readyState	Holds the status of the XMLHttpRequest.
+      // 0: request not initialized
+      // 1: server connection established
+      // 2: request received
+      // 3: processing request
+      // 4: request finished and response is ready
       if (this.readyState == 4 && this.status == 200) {
         handleResponse(this.responseText);
       }
@@ -26,18 +24,15 @@
   };
 
   let handleResponse = text => {
-    results = JSON.parse(text).data.results;
-    writeTable(results.map(item => createRow(item)));
+    results = JSON.parse(text).data.results; //Array of heros
+    allHeros = JSON.parse(text).data.results; //Array of heros
+
+    writeTable(results);
   };
 
-  let writeTable = rows => {
+  let writeTable = herosList => {
+    let rows = herosList.map(hero => rowTemplate(hero)); //Array of rows
     table.innerHTML = rows.join("");
-  };
-  let registerSort = () => {
-    let sortBtns = document.querySelectorAll(".sortBtn");
-    sortBtns.forEach(btn => {
-      btn.onclick = e => sortList(e.target.innerText);
-    });
   };
 
   let sortList = col => {
@@ -63,8 +58,46 @@
         );
         break;
     }
-    writeTable(results.map(item => createRow(item)));
+    writeTable(results);
+  };
+
+  let registerSortBtn = () => {
+    document.querySelectorAll(".searchField").forEach(input => {
+      input.oninput = e => filterList(e.target.id, e.target.value);
+    });
+
+    document.querySelectorAll(".sortBtn").forEach(btn => {
+      btn.onclick = e => sortList(e.target.innerText);
+    });
+  };
+
+  let filterList = (id, searchstr) => {
+    let field = id.replace("hero_", "");
+
+    results = allHeros.filter(hero => {
+      if (field === "returned") {
+        return hero.comics.returned <= searchstr;
+      } else {
+        return (
+          hero[field]
+            .toString()
+            .toLowerCase()
+            .indexOf(searchstr.toLowerCase()) != -1
+        );
+      }
+    });
+    writeTable(results);
   };
 
   getData();
+  registerSortBtn();
+
+  let rowTemplate = hero => {
+    return `<tr>
+  <td>${hero.id}</td>
+  <td>${hero.name}</td>
+  <td>${hero.description}</td>
+  <td>${hero.comics.returned}</td>
+</tr>`;
+  };
 })();
